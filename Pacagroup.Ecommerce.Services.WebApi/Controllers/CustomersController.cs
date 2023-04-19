5e6f177622b15b7775af8b2e6c3abd09bdecb1ad
Juan David Posada Paba <jposada@consware.com.co>
2023-04-19 14:46:56 -0500

    Implementación de capa de servicios pendiente

---
+++ Pacagroup.Ecommerce.Services.WebApi/Controllers/CustomersController.cs
@@ -0,0 +1,183 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Pacagroup.Ecommerce.Application.DTO;
+using Pacagroup.Ecommerce.Application.Interface;
+
+namespace Pacagroup.Ecommerce.Services.WebApi.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CustomersController : Controller
+    {
+        private readonly ICustomersApplication _customerApplication;
+
+        public CustomersController(ICustomersApplication customerApplication) { 
+            _customerApplication = customerApplication;
+        }
+
+        #region "Métodos Sincronos"
+
+
+        [HttpPost]
+        public IActionResult Insert([FromBody]CustomersDTO customersDTO)
+        {
+            if (customersDTO == null) 
+            {
+                return BadRequest();
+            }
+            var response = _customerApplication.Insert(customersDTO);
+
+            if (response.IsSuccess) 
+            { 
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] CustomersDTO customersDTO)
+        {
+            if (customersDTO == null)
+            {
+                return BadRequest();
+            }
+            var response = _customerApplication.Update(customersDTO);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+
+        [HttpDelete("{customerId}")]
+        public IActionResult Delete(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+            {
+                return BadRequest();
+            }
+            var response = _customerApplication.Delete(customerId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId}")]
+        public IActionResult Get(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+            {
+                return BadRequest();
+            }
+            var response = _customerApplication.Get(customerId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var response = _customerApplication.GetAll();
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+        #endregion
+
+
+        #region "Métodos Asincronos"
+
+        [HttpPost]
+        public async Task<IActionResult> InsertAsync([FromBody] CustomersDTO customersDTO)
+        {
+            if (customersDTO == null)
+            {
+                return BadRequest();
+            }
+            var response = await _customerApplication.InsertAsync(customersDTO);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateAsync([FromBody] CustomersDTO customersDTO)
+        {
+            if (customersDTO == null)
+            {
+                return BadRequest();
+            }
+            var response = await _customerApplication.UpdateAsync(customersDTO);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+
+
+        [HttpDelete("{customerId}")]
+        public async Task<IActionResult> DeleteAsync(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+            {
+                return BadRequest();
+            }
+            var response = await _customerApplication.DeleteAsync(customerId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet("{customerId}")]
+        public async Task<IActionResult> GetAsync(string customerId)
+        {
+            if (string.IsNullOrEmpty(customerId))
+            {
+                return BadRequest();
+            }
+            var response = await _customerApplication.GetAsync(customerId);
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response.Message);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var response = await _customerApplication.GetAllAsync();
+
+            if (response.IsSuccess)
+            {
+                return Ok(response);
+            }
+            return BadRequest(response);
+        }
+
+
+        #endregion
+
+    }
+
+
+}
