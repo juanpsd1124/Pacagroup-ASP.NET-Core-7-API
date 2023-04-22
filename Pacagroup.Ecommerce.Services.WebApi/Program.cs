fd38d75bb34435abe551277fcd841060d817fd34
Juan Posada <juanpsd1224@hotmail.com>
2023-04-22 18:44:48 -0500

    Se refactoriza codigo para eliminar clase Startup y migrar a clase Program

--- Pacagroup.Ecommerce.Services.WebApi/Program.cs
+++ Pacagroup.Ecommerce.Services.WebApi/Program.cs
@@ -1,21 +1,48 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿using HealthChecks.UI.Client;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Authentication;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Features;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.HealthCheck;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Injection;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Mapper;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Validator;
+using Pacagroup.Ecommerce.Services.WebApi.Modules.Versioning;
 
-namespace Pacagroup.Ecommerce.Services.WebApi
+var builder = WebApplication.CreateBuilder(args);
+
+//Agregar servicios a contenedor de dependencias
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddMapper();
+builder.Services.AddFeature(builder.Configuration);
+builder.Services.AddInjection(builder.Configuration);
+builder.Services.AddAuthentication(builder.Configuration);
+builder.Services.AddVersioning();
+builder.Services.AddValidator();
+builder.Services.AddHealthCheck(builder.Configuration);
+
+var app = builder.Build();
+
+//Configura Http Request Pipeline
+
+if (app.Environment.IsDevelopment()) 
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    app.UseDeveloperExceptionPage();
+    
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+};
 
+app.UseHttpsRedirection();
+app.UseCors("policyApiEcommerce");
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.MapHealthChecksUI();
+
+app.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
+{
+    Predicate = _ => true,
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+
+app.Run();
