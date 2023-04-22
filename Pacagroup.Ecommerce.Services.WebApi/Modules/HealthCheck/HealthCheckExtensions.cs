d16b85a02c12c9121a603eefdf35ec773c944d96
Juan Posada <juanpsd1224@hotmail.com>
2023-04-22 11:11:40 -0500

    Se implementa funcionalidad Health Check

---
+++ Pacagroup.Ecommerce.Services.WebApi/Modules/HealthCheck/HealthCheckExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Pacagroup.Ecommerce.Services.WebApi.Modules.HealthCheck
+{
+    public static class HealthCheckExtensions
+    {
+        public static IServiceCollection AddHealthCheck(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddHealthChecks()
+                .AddSqlServer(configuration.GetConnectionString("NorthwindConnection"), tags: new[] { "database" })
+                .AddCheck<HealthCheckCustom>("HealthCheckCustom", tags: new[] { "custom" });
+
+            services.AddHealthChecksUI().AddInMemoryStorage();
+
+            return services;
+        }
+    }
+}
